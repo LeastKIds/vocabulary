ab9129f70f9e9c100063f83a85602716f4c14eb4
KRHDY <1901322@g.yju.ac.kr>
2021-07-23 16:47:44 +0900

    황대영 : 뷰 업로드 **주의** npm install 하고 npm run dev 로 해야됨

---
+++ vocabulary-vue/src/router/index.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/Login'
+import SignUp from '@/components/SignUp'
+Vue.use(Router)
+
+export default new Router({
+  mode:'history',
+  routes: [
+    {
+      path: '/hello',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path:'/signup',
+      name:'SignUp',
+      component: SignUp
+    }
+
+  ]
+})
