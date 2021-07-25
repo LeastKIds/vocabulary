e6acd5e98fbe808e4df99a1d2341bc2495298b4a
leastkids <leastkids@gmail.com>
2021-07-25 23:22:14 +0900

    김진홍: 단어저장 validate 수정중

--- vocabulary-laravel/app/Http/Controllers/WordController.php
+++ vocabulary-laravel/app/Http/Controllers/WordController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Word;
 use Illuminate\Http\Request;
+use Validator;
 
 class WordController extends Controller
 {
@@ -15,30 +16,49 @@ class WordController extends Controller
 
     public function store(Request $request) {
 
-        $request -> validate([
-            'chinese_character' => 'required',
-            'hiragana' => 'required',
-            'korean' => 'required',
-            'user_id' => 'required',
-            'vocabulary_id' => 'required'
-        ]);
 
-        $chinese_character = $request -> chinese_character;
-        $hiragana = $request -> hiragana;
-        $korean = $request -> korean;
-        $user_id = $request -> user_id;
-        $vocabulary_id = $request -> vocabulary_id;
 
-        $word = new Word;
-        $word -> chinese_character = $chinese_character;
-        $word -> hiragana = $hiragana;
-        $word -> korean = $korean;
-        $word -> user_id = $user_id;
-        $word -> vocabulary_id = $vocabulary_id;
+//        for($i=0; $request[$i] != null; $i += 1)
+//        {
+//
+//            $request -> validate([
+//                'chinese_character' => 'required',
+//                'hiragana' => 'required',
+//                'korean' => 'required',
+//                'user_id' => 'required',
+//                'vocabulary_id' => 'required'
+//            ]);
+//
+//            $chinese_character = $request[$i] -> chinese_character;
+//            $hiragana = $request[$i] -> hiragana;
+//            $korean = $request[$i] -> korean;
+//            $user_id = $request[$i] -> user_id;
+//            $vocabulary_id = $request[$i] -> vocabulary_id;
+//
+//            $word = new Word;
+//            $word -> chinese_character = $chinese_character;
+//            $word -> hiragana = $hiragana;
+//            $word -> korean = $korean;
+//            $word -> user_id = $user_id;
+//            $word -> vocabulary_id = $vocabulary_id;
+//
+//            $word -> save();
+//        }
 
-        $word -> save();
 
-        return response('word created');
+        $words = $request -> words;
+        $attributes = [
+            'korea' => ['required'],
+        ];
+        $validator = $request -> validate($attributes);
+
+        if($validator -> fails()) {
+            return response('failed');
+        }
+
+
+
+        return response($words[0]);
     }
 
     public function delete() {
