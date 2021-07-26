170d5d5d0592a6e81ae2bbfe3ba0844062389ef9
leastkids <leastkids@gmail.com>
2021-07-26 22:13:18 +0900

    김진홍: 단어추가 기능(정규식은 구현 안 함)

--- vocabulary-laravel/app/Http/Controllers/WordController.php
+++ vocabulary-laravel/app/Http/Controllers/WordController.php
@@ -16,11 +16,11 @@ class WordController extends Controller
 
     public function store(Request $request) {
 
+        $data = $request -> json() -> all();
 
+        for($i=0; $i < count($data); $i += 1)
+        {
 
-//        for($i=0; $request[$i] != null; $i += 1)
-//        {
-//
 //            $request -> validate([
 //                'chinese_character' => 'required',
 //                'hiragana' => 'required',
@@ -28,37 +28,26 @@ class WordController extends Controller
 //                'user_id' => 'required',
 //                'vocabulary_id' => 'required'
 //            ]);
-//
-//            $chinese_character = $request[$i] -> chinese_character;
-//            $hiragana = $request[$i] -> hiragana;
-//            $korean = $request[$i] -> korean;
-//            $user_id = $request[$i] -> user_id;
-//            $vocabulary_id = $request[$i] -> vocabulary_id;
-//
-//            $word = new Word;
-//            $word -> chinese_character = $chinese_character;
-//            $word -> hiragana = $hiragana;
-//            $word -> korean = $korean;
-//            $word -> user_id = $user_id;
-//            $word -> vocabulary_id = $vocabulary_id;
-//
-//            $word -> save();
-//        }
 
+            $words = $data[$i];
+            $chinese_character = $words['chinese_character'];
+            $hiragana = $words['hiragana'];
+            $korean = $words['korean'];
+            $user_id = $words['user_id'];
+            $vocabulary_id = $words['vocabulary_id'];
 
-        $words = $request -> words;
-        $attributes = [
-            'korea' => ['required'],
-        ];
-        $validator = $request -> validate($attributes);
+            $word = new Word;
+            $word -> chinese_character = $chinese_character;
+            $word -> hiragana = $hiragana;
+            $word -> korean = $korean;
+            $word -> user_id = $user_id;
+            $word -> vocabulary_id = $vocabulary_id;
 
-        if($validator -> fails()) {
-            return response('failed');
+            $word -> save();
         }
 
 
-
-        return response($words[0]);
+        return response('save words');
     }
 
     public function delete() {
