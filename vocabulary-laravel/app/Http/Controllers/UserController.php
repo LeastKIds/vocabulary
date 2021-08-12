48c6e634993dd8afc127cfd1436d4b61a7db0c96
leastkids <leastkids@gmail.com>
2021-08-12 12:36:24 +0900

    김진홍: README 로그인 체크 양식

--- vocabulary-laravel/app/Http/Controllers/UserController.php
+++ vocabulary-laravel/app/Http/Controllers/UserController.php
@@ -10,9 +10,9 @@ class UserController extends Controller
     public function checkLogin() {
         $user = auth() -> user();
         if($user!=null)
-            $data=['login' => 1];
+            $data=['login' => 1, 'id' => $user['id']];
         else
-            $data=['login' => 0];
+            $data=['login' => 0, 'id' => null];
         return response() -> json($data);
     }
 }
