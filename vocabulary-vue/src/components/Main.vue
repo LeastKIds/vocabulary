86243a9bd17ff94a6a0e4f0188c39446fb4a4836
AZMIRAI <1901322@g.yju.ac.kr>
2021-08-16 18:32:52 +0900

    황대영 : 아이콘편집 날라간거 복구

--- vocabulary-vue/src/components/Main.vue
+++ vocabulary-vue/src/components/Main.vue
@@ -2,7 +2,8 @@
   <div class="main">
     <v-container >
       <v-layout column>
-      <!-- 타이틀 영역 -->
+      <!-- 타이틀 영역 --><input type="text" v-model="user">
+  <input type="text" v-model="comment">
       <br>
       <v-flex>
       </v-flex>
@@ -11,12 +12,45 @@
           <v-layout row >
             <!-- 2단패널 의 좌측 -->
             <v-flex xs8>
-              <v-btn v-if="btnshow" v-on:click="logout" >로그아웃</v-btn>
-              <router-link v-else to="/signin" style="text-decoration: none"><v-btn >로그인</v-btn></router-link>
-              <v-btn> 공개 단어장 </v-btn>
-              <v-btn> 검색 </v-btn>
-              <v-btn> 정렬 </v-btn>
-      <v-card>
+              <!-- <v-btn v-if="btnshow" v-on:click="logout" >로그아웃</v-btn>
+              <router-link v-else to="/signin" style="text-decoration: none"><v-btn >로그인</v-btn></router-link> -->
+             <!-- 버튼! -->
+            <v-btn
+              color="yellow"
+              elevation="5"
+              rounded
+              v-if="btnshow" v-on:click="logout"
+              >로그아웃</v-btn>
+              <router-link v-else to="/signin" style="text-decoration: none">
+              <v-btn >로그인</v-btn></router-link>
+              
+            &nbsp;
+            <v-btn
+              color="accent"
+              elevation="5"
+              rounded
+            >공개 단어장</v-btn>
+            &nbsp;
+            <v-btn
+              color="accent"
+              elevation="5"
+              rounded
+            >검색</v-btn>
+            &nbsp;
+            <v-btn
+              color="accent"
+              elevation="5"
+              rounded
+            >정렬</v-btn>
+            &nbsp;
+
+
+      <br>
+      <br>
+    <ul>
+              <li v-for="detail in details" :key="detail.id">{{ detail.info }}</li>
+            </ul>
+      <!-- <v-card>
       <v-list-item one-line >
       <v-list-item-content>
         <v-list-item-title class="text-h5 mb-1">
@@ -35,7 +69,7 @@
         Button
       </v-btn>
     </v-card-actions>
-  </v-card>
+  </v-card> -->
              </v-flex>
 
 
@@ -44,9 +78,48 @@
 
           <v-flex low>
           <v-layout column>
-          <v-btn> 추가 </v-btn>
-          <v-btn> 삭제 </v-btn>
-          <v-btn> 수정 </v-btn>    
+  <v-btn
+              class="mx-2"
+              fab
+              dark
+              large
+              elevation="5"
+              color="indigo"
+              v-on:click="vo_plus"
+             >
+              <v-icon dark>
+              mdi-plus
+              </v-icon>
+            </v-btn>
+            &nbsp;
+
+            <v-btn
+              class="mx-2"
+              fab
+              dark
+              large
+              color="cyan"
+            >
+              <v-icon dark>
+              mdi-pencil
+              </v-icon>
+           </v-btn>
+           &nbsp;
+
+            <v-btn
+              class="mx-2"
+              fab
+              dark
+              large
+              color="red"
+            >
+              <v-icon dark>
+              mdi-delete
+              </v-icon>
+            </v-btn>
+            &nbsp; 
+             
+            <v-btn @click="save">저장</v-btn><br>
           </v-layout>
           </v-flex>
        
@@ -61,7 +134,7 @@
 
 
         </v-layout>
-        
+           
       </v-flex>
       
     </v-layout>
@@ -80,7 +153,10 @@ export default {
   data() {
     return {
       msg: '메인화면입니다',
-      btnshow: true
+      btnshow: true,
+      user : null,
+      comment : null, 
+      messages:[],
     }
   },
   created() {
@@ -107,8 +183,21 @@ export default {
         .catch(err => {
           console.log(err)
         })
-      }
-    }
+      },
+        vo_plus() {
+        prompt('단어장 제목을 입력하세요~', '무제');
+        }
+      },
+      save(){
+              const message= {
+                  user:this.user,
+                  comment:this.comment,
+              };
+              // 내림차순 unshift
+              this.messages.unshift(message)
+              this.user=null
+              this.comment=null
+          }
   }
 
 </script>
